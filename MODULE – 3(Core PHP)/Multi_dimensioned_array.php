626ababa4a272b4e06ca62c4a6c14693ea3f153a
nishaaya9 <117917278+nishaaya9@users.noreply.github.com>
2023-02-18 11:32:52 +0530

    Update Multi_dimensioned_array.php

--- MODULE – 3(Core PHP)/Multi_dimensioned_array.php
+++ MODULE – 3(Core PHP)/Multi_dimensioned_array.php
@@ -1,3 +1,16 @@
 <?php
-
+$balance = array(
+    array(1.5, 2.8, 4.1,),
+    array(1.5, 2.8, 4.1,),
+    array(1.5, 2.8, 4.1,),
+    array(1.5, 2.8, 4.1,),
+    array(1.5, 2.8, 4.1,));
+    for ($i=0; $i < count($balance); $i++) 
+    {
+        for ($j=0; $j < count($balance[$i]); $j++) 
+        { 
+            echo $balance[$i][$j]."&nbsp;&nbsp;";
+        } 
+        echo "<br>";
+    }
 ?>
