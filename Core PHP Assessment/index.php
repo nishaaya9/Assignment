0b51cef0139c9e70012c41b0990733be8c72f307
nishaaya9 <117917278+nishaaya9@users.noreply.github.com>
2023-03-20 09:59:09 +0530

    Assessment

---
+++ Core PHP Assessment/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>$tudent Management $ystem</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+</head>
+<style>
+    #header {
+        height: 6%;
+        width: 100%;
+        top: 2%;
+        font-size: 25px;
+        background-color: black;
+        color: white;
+        position: absolute;
+    }
+</style>
+
+<body>
+    <center>
+        <div id="header">
+            $tudent Management $ystem
+        </div>
+        <br><br><br>
+        <div>
+            <a href="coun_login.php" class="btn btn-primary">Counsellor Login</a> |
+            <a href="facu_login.php" class="btn btn-primary">Faculty Login</a> |
+            <a href="stud_login.php" class="btn btn-primary">Student login</a>
+        </div>
+    </center>
+</body>
+
+</html>
